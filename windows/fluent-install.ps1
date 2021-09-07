cd9220f4e40019e6c186118c5684087509628d60
hegdesandesh225 <sandesh@logiq.ai>
2021-09-07 16:57:57 +0530

    power shell script for Fluent to Logiq

---
+++ windows/fluent-install.ps1
@@ -0,0 +1,14 @@
+Invoke-WebRequest 'https://fluentbit.io/releases/1.8/td-agent-bit-1.8.6-win64.zip'  -OutFile td-agent-bit.zip
+Expand-Archive -Path td-agent-bit.zip -DestinationPath .
+$path=ls  *td* -Recurse  -Directory|Select-String td-agent
+$conf='\conf'
+$confpath1=Join-Path $path $conf
+Copy-Item "fluent-bit.conf" -Destination $confpath1
+
+$appendbin='\bin\fluent-bit.exe'
+$binpath=Join-Path $path $appendbin
+$appendconf='\conf\fluent-bit.conf'
+$confpath=Join-Path $path $appendconf
+
+sc.exe create fluent-bit binpath= "$binpath -c $confpath" start= auto
+net start fluent-bit
