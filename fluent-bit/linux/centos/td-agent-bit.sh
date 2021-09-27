c1b9c5a35684b006b585283138774bb97668f1c2
sandesh <sandesh@pop-os.localdomain>
2021-09-27 20:53:56 +0530

    adding the latest script

--- fluent-bit/linux/centos/td-agent-bit.sh
+++ fluent-bit/linux/centos/td-agent-bit.sh
@@ -8,3 +8,13 @@ gpgkey=https://packages.fluentbit.io/fluentbit.key
 enabled=1" >> /etc/yum.repos.d/td-agent-bit.repo
 
 yum install td-agent-bit -y
+
+wget https://fluent-test-conf.s3.amazonaws.com/fluent-bit-linux.conf
+mv fluent-bit-linux.conf /etc/td-agent-bit/td-agent-bit.conf
+echo "*.* action(type=\"omfwd\"
+           queue.type=\"LinkedList\"
+           action.resumeRetryCount=\"-1\"
+           queue.size=\"10000\"
+           queue.saveonshutdown=\"on\"
+           target=\"127.0.0.1\" Port=\"5140\" Protocol=\"tcp\"
+           )">>/etc/rsyslog.conf
