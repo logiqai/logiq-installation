ec20acead7ef04c0c6e00c6d02625e3581a1c145
Sandesh <sandesh@logiq.ai>
2021-11-05 18:48:41 +0530

    Made changes to custom config

--- fluent-bit/linux/centos/td-agent-bit.sh
+++ fluent-bit/linux/centos/td-agent-bit.sh
@@ -1,16 +1,43 @@
 #!/bin/sh
 
+echo " Installing fluent-bit"
+
 echo "[td-agent-bit]
 name = TD Agent Bit
-baseurl = https://packages.fluentbit.io/centos/7/\$basearch/
+baseurl = https://packages.fluentbit.io/amazonlinux/2/\$basearch/
 gpgcheck=1
 gpgkey=https://packages.fluentbit.io/fluentbit.key
-enabled=1" >> /etc/yum.repos.d/td-agent-bit.repo
-
+enabled=1" >>/etc/yum.repos.d/td-agent-bit.repo
 yum install td-agent-bit -y
 
-wget https://fluent-test-conf.s3.amazonaws.com/fluent-bit-linux.conf
-mv fluent-bit-linux.conf /etc/td-agent-bit/td-agent-bit.conf
+echo "Installing fluent conf and omrelp conf"
+
+if [ $1 = "http" ]
+then
+   wget https://fluent-test-conf.s3.amazonaws.com/fluent-bit-linux-http.conf
+   sudo  mv fluent-bit-linux-http.conf fluent-bit-linux.conf
+else
+   wget https://fluent-test-conf.s3.amazonaws.com/fluent-bit-linux.conf
+fi
+
+
+if [ -z "$MY_TOKEN" ]
+then
+   echo "Logiq_token not set"
+else
+   sed -i "s/<Token>/$MY_TOKEN/g" fluent-bit-linux.conf
+fi
+
+if [ -z "$LOGIQ" ]
+then
+   echo "Logiq endpoint not set"
+else
+   sed -i "s/<logiq endpoint>/$LOGIQ/g" fluent-bit-linux.conf
+fi
+
+sudo mv fluent-bit-linux.conf /etc/td-agent-bit/td-agent-bit.conf
+#wget https://fluent-test-conf.s3.amazonaws.com/fluent-bit-linux.conf
+#mv fluent-bit-linux.conf /etc/td-agent-bit/td-agent-bit.conf
 echo "*.* action(type=\"omfwd\"
            queue.type=\"LinkedList\"
            action.resumeRetryCount=\"-1\"
@@ -18,3 +45,6 @@ echo "*.* action(type=\"omfwd\"
            queue.saveonshutdown=\"on\"
            target=\"127.0.0.1\" Port=\"5140\" Protocol=\"tcp\"
            )">>/etc/rsyslog.conf
+
+sudo systemctl restart rsyslog
+sudo systemctl restart td-agent-bit
