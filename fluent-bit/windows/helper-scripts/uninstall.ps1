6d6a0f0381d8daa34411e8a9fbbf5c0f8ae0c1e2
Kumar Vishnu <kumarvishnu@logiq.ai>
2022-12-27 01:29:54 +0530

    changed service name to fluent-bit

--- fluent-bit/windows/helper-scripts/uninstall.ps1
+++ fluent-bit/windows/helper-scripts/uninstall.ps1
@@ -1,5 +1,5 @@
 Write-Host "Uninstalling fluent-bit..." -BackgroundColor Red -ForegroundColor White
 Stop-Service fluent-bit -PassThru
-C:\Windows\System32\sc.exe delete ServiceName
+C:\Windows\System32\sc.exe delete fluent-bit
 rm ./td-agent-bit-1.8.6-win64
 rm td-agent-bit.zip
