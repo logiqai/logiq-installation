f48802c15def9340226247939b31ab8c6372e2e8
Kumar Vishnu <kumarvishnu@logiq.ai>
2022-12-27 16:54:02 +0530

    added descending order in list channels

--- fluent-bit/windows/helper-scripts/listChannels.ps1
+++ fluent-bit/windows/helper-scripts/listChannels.ps1
@@ -1,5 +1,5 @@
 # to see channels listed in the standard order
-Get-WinEvent -ListLog *
+# Get-WinEvent -ListLog *
 
 # to sort more active channels to the top of the list
-# Get-WinEvent -ListLog * | sort RecordCount -Descending
+Get-WinEvent -ListLog * | sort RecordCount -Descending
